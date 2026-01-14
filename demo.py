37e716cece741a4eac13784f3887e503c4fbe16f
oshry <oshry70@gmail.com>
2026-01-14 21:06:38 +0200

    add  test def

--- demo.py
+++ demo.py
@@ -2,11 +2,11 @@ from api_client.client import APIClient
 from api_client.utils import pretty_print
 
 def main():
-    client = APIClient("https://jsonplaceholder.typicode.com")
+    client = APIClient("https://jsonplaceholder.typicode.com/")
 
     # GET request example
     print("Fetching first 3 posts:")
-    posts = client.get_data("posts")[:3]
+    posts = client.get_data("posts")[:4]
     pretty_print(posts)
 
     # POST request example
@@ -19,5 +19,9 @@ def main():
     response = client.post_data("posts", payload)
     pretty_print(response)
 
+def test_asher():
+    print("this is test from asher")
+
+test_asher()
 if __name__ == "__main__":
     main()
